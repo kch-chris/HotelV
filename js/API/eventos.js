5de1d94674518d1bc182d8ca1e3db9d42b2410ff
Christian Murillo <christian.murillo@nuvemtecnologia.mx>
2013-04-19 20:56:43 -0500

    cambios

---
+++ js/API/eventos.js
@@ -0,0 +1,28 @@
+// Eventos
+$(document).ready(function(){
+	document.addEventListener("deviceready",function(){
+	$('#regEnviar').tap(function(){
+		var nom=$('#regNom').val();
+		var email=$('#regEmail').val();
+		var tel=$('#regTel').val();
+		if(nom!='' && email!='' && tel!='')
+		{
+			navigator.notification.confirm("Nombre:"+nom+"\nEmail:"+email+"\nTelefono:"+tel,function(botones){
+		switch(botones){
+		case 1:
+		navigator.notification.beep(5);
+		break;
+		case 2:
+		navigator.notification.vibrate(500);
+		break;
+		}
+	},"Titulo","Beep,Vibrar,Salir");
+			
+			}
+		else 
+			{
+				navigator.notification.alert('Todos los campos son requeridos',null,"Error de Registro","Aceptar");
+				}
+		});},false);
+	});
+	
